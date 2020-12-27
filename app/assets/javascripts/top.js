f2be0cec6fe4173976567e6f1a0ac389c5cd7fd6
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-12-27 16:52:13 +0900

    signin_page

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,3 +1,29 @@
 $(function(){
-  
+
+  setTimeout(function(){
+    $('.SigninPage__mask').animate({
+      'width': '0vh'
+    }, 500);
+  }, 500);
+
+  setTimeout(function(){
+    $('.SigninPage__image__contents__line').animate({
+      'height': '350px'
+    }, 500, function(){
+      $('.SigninPage__image__contents__line__title').animate({
+        'opacity': '1'
+      }, 300)
+    });
+  }, 1000);
+
+  $('.SigninPage__image__contents__line__title__line3').click(function(){
+    $('.SigninPage__image__contents__line').animate({
+      'width': '1100px'
+    }, 500, function(){
+      $('.SigninPage__image__contents__line__right.display-none').removeClass('display-none')
+      $('.SigninPage__image__contents__line__right').animate({
+        'opacity': '1'
+      }, 1000)
+    })
+  });
 });
