17d685cb0d1b8d203d6b9a032b0ee54457258450
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-12-29 20:28:55 +0900

    image_delete

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,6 +1,6 @@
 class Cost < ApplicationRecord
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   
   validates :dating, presence: true
